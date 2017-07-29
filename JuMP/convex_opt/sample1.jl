57da86e3287f1d958153a5a3118fa5ce7cd92b71
Atsushi Sakai <asakai.amsl+github@gmail.com>
2017-07-29 16:36:01 -0700

    sample1.jl is first release

---
+++ JuMP/convex_opt/sample1.jl
@@ -0,0 +1,25 @@
+using JuMP
+using Mosek
+
+function main()
+    println("JuMP sample1")
+    model = Model(solver=MosekSolver())
+    m = 10
+    n = 5
+    A = rand(m, n)
+    b = rand(m, 1)
+    println(A)
+    println(b)
+
+    @variable(model, 0.0<=x[1:size(A,2)]<=1.0)
+    @objective(model, Min, sum((A*x-b).^2))
+
+    println("The optimization problem to be solved is:")
+    println(model) # Shows the model constructed in a human-readable form
+
+    status = solve(model)
+    println("Objective value: ", getobjectivevalue(model)) 
+    println("x:",getvalue(x))
+end
+
+main()
