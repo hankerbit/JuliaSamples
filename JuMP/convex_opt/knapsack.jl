128f767e4cd3b62bfd820e18f6458ff28f4145dd
Atsushi Sakai <asakai.amsl+github@gmail.com>
2017-07-30 09:47:36 -0700

    add knapsack sample

---
+++ JuMP/convex_opt/knapsack.jl
@@ -0,0 +1,27 @@
+using JuMP
+using Mosek
+
+function main()
+    println("JuMP knapsack")
+    model = Model(solver=MosekSolver())
+
+    size = [21, 11, 15, 9, 34, 25, 41, 52]
+    weight = [22, 12, 16, 10, 35, 26, 42, 53]
+    capacity = 100
+
+    @variable(model, x[1:length(size)], Bin)
+    @objective(model, Max, dot(weight,x))
+    @constraint(model, dot(size, x) <= capacity)
+
+    println("The optimization problem to be solved is:")
+    println(model) # Shows the model constructed in a human-readable form
+
+    status = solve(model)
+    println("Objective value: ", getobjectivevalue(model)) 
+    println("Solution is:")
+    for i = 1:length(size)
+        println("x[$i] = ", getvalue(x[i]))
+    end
+end
+
+main()
