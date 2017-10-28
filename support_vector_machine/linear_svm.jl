552773c3954be3e4b1fb71dcd48794b2ab71d399
Atsushi Sakai <asakai.amsl+github@gmail.com>
2017-10-28 16:01:09 -0700

    first release linear support vector machine

--- support_vector_machine/linear_svm.jl
+++ support_vector_machine/linear_svm.jl
@@ -6,21 +6,53 @@
 
 using PyCall
 using Distributions
+using JuMP
+using CPLEX
 
+solver = CplexSolver(CPX_PARAM_SCRIND=0)
 @pyimport matplotlib.pyplot as plt
+
+const λ = 1.0
+
+function lsvm(x, y)
+
+    model = Model(solver=solver)
+    @variable(model, w[1:length(x[1,:])])
+    @variable(model, b)
+    @variable(model, s[1:length(x[:,1])] >= 0.0)
+
+    for i in 1:length(x[:,1])
+        @constraint(model, (y[i]*(w'*x[i,:] - b)) -1 >= -s[i])
+    end
+
+    obj = w'*w + λ*sum(s)
+
+    @objective(model, Min, obj)
+
+    status = solve(model)
+ 
+    w_vec = getvalue(w)
+    b_vec = getvalue(b)
+
+    println("w:",w_vec)
+    println("b:",b_vec)
+
+    return w_vec, b_vec
+end
 
 
 function main()
     println(PROGRAM_FILE," start!!")
 
-    d1 = rand(Normal(1, 2), 100, 2)
-    d2 = rand(Normal(-2, 1), 100, 2)
-
+    d1 = hcat(rand(Normal(-1, 2), 100), rand(Normal(5, 2), 100))
+    d2 = hcat(rand(Normal(1, 2), 100), rand(Normal(2, 2), 100))
     d = vcat(d1,d2)
-    # println(size(d))
+    c = vcat([1 for i in 1:100], [-1 for i in 1:100])
 
-    lsvm(d)
+    w, b = lsvm(d, c)
 
+    seq = [i for i in minimum(d[:,1]):0.1:maximum(d[:,1])]
+    plt.plot(seq, -(w[1] * seq - b)/ w[2] , "-k")
     plt.plot(d1[:,1],d1[:,2],"or")
     plt.plot(d2[:,1],d2[:,2],"ob")
     plt.axis("equal")
