d94f73f27abf8144ecafe8e4b9244b8b82fcc7dc
Atsushi Sakai <asakai.amsl@gmail.com>
2018-07-24 09:18:27 +0900

    polynomial_fitting is added

---
+++ least_square/polynomial_fitting.jl
@@ -0,0 +1,64 @@
+using PyPlot
+
+
+function get_sin_training()
+
+    x = [i for i in 0:0.5:10]
+
+    y = sin.(x) + x
+
+
+    return x, y
+end
+
+
+function construct_polynomial_matrix(tx, degree)
+    nteaching=length(tx)
+    A = fill(1.0, (nteaching,1))
+    for i in 1:degree
+        At = tx.^i
+        A = hcat(A, At)
+    end
+    #  display(A)
+
+    return A
+end
+
+
+function polynomial_fitting(tx, ty, degree, x)
+
+    A = construct_polynomial_matrix(tx, degree)
+    #  display(A)
+
+    # calc parameter vector
+    pv = inv(A'*A)*A'*ty
+
+    Ap = construct_polynomial_matrix(x, degree)
+
+    y = Ap*pv 
+    
+    return y
+end
+
+
+function main()
+
+    tx, ty = get_sin_training()
+
+    x = [i for i in 0:0.1:10]
+    plot(tx, ty, "xb")
+
+    for d in 1:10
+        y = polynomial_fitting(tx, ty, d, x)
+        plot(x, y)
+    end
+
+    axis("equal")
+
+    show()
+
+end
+
+
+main()
+
