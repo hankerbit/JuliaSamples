46b531466d9ac5ababd73246531be1952c82eac4
Atsushi Sakai <asakai.amsl@gmail.com>
2018-09-03 14:05:03 +0900

    for release

--- least_square/portfolio_optimization.jl
+++ least_square/portfolio_optimization.jl
@@ -11,13 +11,12 @@ init_value = 1000
 r = mu .+ sigma*randn(T);
 v = cum_value(r, init_value);
 # compare final value (compounded) and average return
-println(v[T] , ",", v[1]*(1+sum(r)))
+# println(v[T] , ",", v[1]*(1+sum(r)))
 # plot(1:T, v)
 
 function port_opt(R,rho)
 	T, n = size(R)
 	mu = sum(R, 1)'/T
-	println(mu)
 	KKT = [ 2*R'*R ones(n) mu; ones(n)' 0 0; mu' 0 0]
 	wz1z2 = KKT \ [2*rho*T*mu; 1; rho]
 	w = wz1z2[1:n]
@@ -30,22 +29,24 @@ stdev(x) = rms(x .- avg(x))
 
 include("./VMLS.jl/src/portfolio_data.jl")
 R, RTest = portfolio_data()
+plot(R)
+
 T, n = size(R)
 println(T,",", n)
 rho = 0.10/250 # Ask for 10% annual return
 w = port_opt(R,rho)
 r = R*w; # Portfolio return time series
-plot(1:T, cum_value(r, init_value), label= "10%")
+# plot(1:T, cum_value(r, init_value), label= "10%")
 
 rho = 0.5/250 # Ask for 10% annual return
 w = port_opt(R,rho)
 r = R*w; # Portfolio return time series
-plot(1:T, cum_value(r, init_value), label= "5%")
+# plot(1:T, cum_value(r, init_value), label= "5%")
 
 rho = 0.01/250 # Ask for 10% annual return
 w = port_opt(R,rho)
 r = R*w; # Portfolio return time series
-plot(1:T, cum_value(r, init_value), label= "1%")
+# plot(1:T, cum_value(r, init_value), label= "1%")
 legend()
 
 show()
