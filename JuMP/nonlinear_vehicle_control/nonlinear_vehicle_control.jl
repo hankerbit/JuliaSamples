b6190b13704b8867caf9ffcd1eead2531afa61a8
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-11-03 21:33:04 +0900

    update

--- JuMP/nonlinear_vehicle_control/nonlinear_vehicle_control.jl
+++ JuMP/nonlinear_vehicle_control/nonlinear_vehicle_control.jl
@@ -3,8 +3,8 @@
 #
 # author: Atsushi Sakai
 #
-# see: 	Boyd, Stephen, and Lieven Vandenberghe.
-# 		Introduction to Applied Linear Algebra: Vectors, Matrices, and Least Squares.
+# see:	Boyd, Stephen, and Lieven Vandenberghe.
+#		Introduction to Applied Linear Algebra: Vectors, Matrices, and Least Squares.
 #		Cambridge University Press, 2018. P429
 #
 
@@ -15,23 +15,23 @@ using PyPlot
 function solve_nonlinear_vehicle_control(xfinal, N, gamma, L, dt)
 
 	solver=IpoptSolver(print_level=0)
-    model = Model(solver=solver)
+	model = Model(solver=solver)
 
 	nx = 3
 	nu = 2
 
-    @variable(model, x[k=1:nx,t=1:N])
-    @variable(model, u[k=1:nu,t=1:N])
+	@variable(model, x[k=1:nx,t=1:N])
+	@variable(model, u[k=1:nu,t=1:N])
 
 	# initial and terminal constraint 
 	@constraint(model, x[:,1] .== [0.0; 0.0; 0.0])
 	@constraint(model, x[:,N] .== xfinal)
 
 	initx = zeros(nx, N)
-    for k in 1:N-1
+	for k in 1:N-1
 		# vehicle model
 		@NLconstraint(model,  x[1,k+1] == x[1,k] + dt*u[1,k]*cos(x[3,k]))
-        @NLconstraint(model,  x[2,k+1] == x[2,k] + dt*u[1,k]*sin(x[3,k]))
+		@NLconstraint(model,  x[2,k+1] == x[2,k] + dt*u[1,k]*sin(x[3,k]))
 		@NLconstraint(model,  x[3,k+1] == x[3,k] + dt*u[1,k]/L*tan(u[2,k]))
 
 		initx[:,k] = xfinal*k/N
@@ -43,13 +43,13 @@ function solve_nonlinear_vehicle_control(xfinal, N, gamma, L, dt)
 	obj = sum(sum(u[:, i].^2 for i=1:N))
 	obj += gamma*sum(sum((u[:, i+1]-u[:, i]).^2 for i=1:N-1))
 
-    @objective(model, Min, obj)
+	@objective(model, Min, obj)
 
-    status = solve(model)
+	status = solve(model)
 	println(status)
 
 	x_opt = getvalue(x)
-    u_opt = getvalue(u)
+	u_opt = getvalue(u)
 
 	display(x_opt)
 	# display(u_opt)
@@ -59,7 +59,7 @@ end
 
 
 function main()
-    println(PROGRAM_FILE," start!!")
+	println(PROGRAM_FILE," start!!")
 
 	xfinal = [-1.0;1.0;deg2rad(90)]
 	N = 50 # control steps
@@ -74,7 +74,7 @@ function main()
 	axis("equal")
 	show()
 
-    println(PROGRAM_FILE," Done!!")
+	println(PROGRAM_FILE," Done!!")
 end
 
 # @time main()
