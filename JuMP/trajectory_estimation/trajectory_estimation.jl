0e92bd03bc70d67b5c79ae9461e6797b84eb39e9
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-11-04 21:21:44 +0900

    first release trajectory estimation

--- JuMP/trajectory_estimation/trajectory_estimation.jl
+++ JuMP/trajectory_estimation/trajectory_estimation.jl
@@ -1,10 +1,62 @@
 #
-# 
+# Trajectory Estimzation with nonlinear optimization
+#
+# Assume the robot can get dead-reckoning and GPS positioning data.
 #
 # author: Atsushi Sakai
 #
 
+using JuMP
+using Ipopt
 using PyPlot
+
+function estimate_trajectory(h_x, h_z, h_u, dt, lamda)
+
+	solver=IpoptSolver(print_level=0)
+	model = Model(solver=solver)
+
+	N = size(h_x)[2]
+	nx = 3
+	nu = 2
+
+	@variable(model, x[k=1:nx,t=1:N])
+	@variable(model, u[k=1:nu,t=1:N])
+
+	# initial constraint 
+	@constraint(model, x[:,1] .== [0.0; 0.0; 0.0])
+
+	obj = 0
+	for k in 1:N-1
+		# vehicle model
+		@NLconstraint(model,  x[1,k+1] == x[1,k] + dt*u[1,k]*cos(x[3,k]))
+		@NLconstraint(model,  x[2,k+1] == x[2,k] + dt*u[1,k]*sin(x[3,k]))
+		@NLconstraint(model,  x[3,k+1] == x[3,k] + dt*u[2,k])
+	end
+	setvalue(x, h_x)# warm starting 
+	setvalue(u, h_u)# warm starting 
+
+	@NLobjective(model, Min, sum(
+				   (x[1,k+1] - (x[1,k] + dt*h_u[1,k]*cos(x[3,k])))^2 
+				 + (x[2,k+1] - (x[2,k] + dt*h_u[1,k]*sin(x[3,k])))^2 
+				 + (x[3,k+1] - (x[3,k] + dt*h_u[2,k]))^2 
+				   for k in 1:N-1)
+				 +lamda*sum(
+				 	(x[1,k] - h_z[1,k])^2 
+				 +  (x[2,k] - h_z[2,k])^2 
+				 for k in 1:N-1 if h_z[1,k] != 0.0
+				 ))
+	
+
+	status = solve(model)
+	println(status)
+
+	x_opt = getvalue(x)
+	u_opt = getvalue(u)
+
+	display(x_opt)
+
+	return x_opt, u_opt
+end
 
 function predict_motion(x, u, dt)
 	x[1]=x[1]+u[1]*dt*cos(x[3])
@@ -40,6 +92,7 @@ function simulate(u, SIMTIME, DT, ZDT, Q, R)
 	h_xTrue = zeros(3,0)
 	h_x = zeros(3,0)
 	h_z = zeros(2,0)
+	h_u = zeros(2,0)
 
 	xTrue = [0.0, 0.0, 0.0]
 	x = [0.0, 0.0, 0.0]
@@ -53,26 +106,32 @@ function simulate(u, SIMTIME, DT, ZDT, Q, R)
 		h_xTrue = hcat(h_xTrue, xTrue)
 		h_x = hcat(h_x, x)
 		h_z = hcat(h_z, z)
-
+		h_u = hcat(h_u, u)
 	end
 
-	return h_xTrue, h_x, h_z
+	return h_xTrue, h_x, h_z, h_u
 end
 
 function main()
     println(PROGRAM_FILE," start!!")
 
 	SIMTIME = 50.0
-	DT = 0.1
-	ZDT = 4.0
+	DT = 0.1 # time tick for control
+	ZDT = 4.0 # time tick to get observation
 	Q = [0.5,0.1] #input noise
 	u = [1.0,0.1]# input v[m/s],omega[rad/s]
 	R=[0.1] #observation noise
 
-	h_xTrue, h_x, h_z = simulate(u, SIMTIME, DT, ZDT, Q, R)
+	h_xTrue, h_x, h_z, h_u = simulate(u, SIMTIME, DT, ZDT, Q, R)
+
+	lamda = 0.1 # weight for observation residual
+	x_opt, u_opt = estimate_trajectory(h_x, h_z, h_u, DT, lamda)
+
+	close()
 	plot(h_xTrue[1,:], h_xTrue[2,:], ".b")
-	plot(h_x[1,:], h_x[2,:], ".r")
+	plot(h_x[1,:], h_x[2,:], ".g")
 	plot(h_z[1,:], h_z[2,:], "xg")
+	plot(x_opt[1,:], x_opt[2,:], "-r")
 	axis("equal")
 
     println(PROGRAM_FILE," Done!!")
